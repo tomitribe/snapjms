43f5acd5b837f20cf7ef5f5fe8d0e4dc96a09306
Jonathan S. Fisher <jfisher@tomitribe.com>
2016-06-13 17:52:11 -0500

    fix unit tests

--- snapjms/src/test/java/org/tomitribe/oss/snapjms/marshalling/JAXBXMLMarshallerTest.java
+++ snapjms/src/test/java/org/tomitribe/oss/snapjms/marshalling/JAXBXMLMarshallerTest.java
@@ -13,12 +13,14 @@ import org.jglue.cdiunit.CdiRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.tomitribe.oss.snapjms.AtResourceInjectionCDIExtension;
+import org.tomitribe.oss.snapjms.api.SnapJMS;
 import org.tomitribe.oss.snapjms.api.jaxbconfig.SnapJMSJAXBXMLMarshallerConfig;
 
 @RunWith(CdiRunner.class)
 @AdditionalClasses({ AtResourceInjectionCDIExtension.class, SnapJMSJAXBXMLMarshallerConfig.class })
 public class JAXBXMLMarshallerTest {
    @Inject
+   @SnapJMS
    private JAXBXMLMarshaller jaxbXMLMarshaller;
 
    @Test
