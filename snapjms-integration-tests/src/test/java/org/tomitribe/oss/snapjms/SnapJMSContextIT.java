fda6fe372fb0ae634d917ed5277d3710b6447df5
Jonathan S. Fisher <jfisher@tomitribe.com>
2016-06-13 09:25:03 -0500

    Add Integration Test for non-transactional bean

--- snapjms-integration-tests/src/test/java/org/tomitribe/oss/snapjms/SnapJMSContextIT.java
+++ snapjms-integration-tests/src/test/java/org/tomitribe/oss/snapjms/SnapJMSContextIT.java
@@ -12,12 +12,16 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.transaction.Status;
 import javax.transaction.UserTransaction;
 
 import org.apache.openejb.junit.jee.EJBContainerRunner;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.tomitribe.oss.snapjms.api.SnapJMSContext;
+import org.tomitribe.oss.snapjms.api.SnapJMSNonTransacted;
 
 @RunWith(EJBContainerRunner.class)
 public class SnapJMSContextIT {
@@ -29,71 +33,83 @@ public class SnapJMSContextIT {
    private UserTransaction utx;
    @Inject
    private SnapJMSContext snapJMSContext;
+   @Inject
+   @SnapJMSNonTransacted
+   private SnapJMSContext snapJMSContext_noTx;
+   private Connection connection;
+
+   @Before
+   public void before() throws Exception {
+      connection = connectionFactory.createConnection();
+      connection.start();
+   }
+
+   @After
+   public void after() throws Exception {
+      connection.close();
+   }
 
    @Test
-   public void test_checkContainer() throws Exception {
-      Connection connection = connectionFactory.createConnection();
-      connection.start();
+   public void test_checkIntegrationContainerSanity() throws Exception {
       try {
-         try {
-            utx.begin();
-            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue(TEST_DESTINATION);
-            MessageProducer producer = session.createProducer(destination);
-            TextMessage message = (TextMessage) session.createTextMessage(TEST_PAYLOAD);
-            producer.send(message);
-         } finally {
-            utx.commit();
-         }
-         TextMessage rxMessage;
-         try {
-            utx.begin();
-            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue(TEST_DESTINATION);
-            MessageConsumer consumer = session.createConsumer(destination);
-            rxMessage = (TextMessage) consumer.receive(1000L);
-         } finally {
-            utx.commit();
-         }
-         if (rxMessage == null) {
-            fail("No message Recieved. That means there's a problem with the Junit/Application Composer setup of this project."
-                  + "This needs to be fixed first before fixing any other tests.");
-         } else {
-            assertEquals(TEST_PAYLOAD, rxMessage.getText());
-         }
+         utx.begin();
+         Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+         Destination destination = session.createQueue(TEST_DESTINATION);
+         MessageProducer producer = session.createProducer(destination);
+         TextMessage message = (TextMessage) session.createTextMessage(TEST_PAYLOAD);
+         producer.send(message);
       } finally {
-         connection.close();
+         utx.commit();
+      }
+      TextMessage rxMessage = receiveMessage();
+      if (rxMessage == null) {
+         fail("No message Recieved. That means there's a problem with the Junit/Application Composer setup of this project."
+               + "This needs to be fixed first before fixing any other tests.");
+      } else {
+         assertEquals(TEST_PAYLOAD, rxMessage.getText());
       }
    }
 
    @Test
    public void testSnapJMSContext_send() throws Exception {
-      Connection connection = connectionFactory.createConnection();
-      connection.start();
       try {
-         try {
-            utx.begin();
-            snapJMSContext.send(TEST_PAYLOAD, TEST_DESTINATION);
-         } finally {
-            utx.commit();
-         }
-         TextMessage rxMessage;
-         try {
-            utx.begin();
-            Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue(TEST_DESTINATION);
-            MessageConsumer consumer = session.createConsumer(destination);
-            rxMessage = (TextMessage) consumer.receive(10L);
-         } finally {
-            utx.commit();
-         }
-         if (rxMessage == null) {
-            fail("No message Recieved");
-         } else {
-            assertEquals(TEST_PAYLOAD, rxMessage.getText());
-         }
+         utx.begin();
+         snapJMSContext.send(TEST_PAYLOAD, TEST_DESTINATION);
       } finally {
-         connection.close();
+         utx.commit();
       }
+      TextMessage rxMessage = receiveMessage();
+      if (rxMessage == null) {
+         fail("No message Recieved");
+      } else {
+         assertEquals(TEST_PAYLOAD, rxMessage.getText());
+      }
+   }
+
+   @Test
+   public void testSnapJMSContext_sendImmediately() throws Exception {
+      assertEquals(Status.STATUS_NO_TRANSACTION, utx.getStatus());
+      snapJMSContext_noTx.send(TEST_PAYLOAD, TEST_DESTINATION);
+      assertEquals(Status.STATUS_NO_TRANSACTION, utx.getStatus());
+      TextMessage rxMessage = receiveMessage();
+      if (rxMessage == null) {
+         fail("No message Recieved");
+      } else {
+         assertEquals(TEST_PAYLOAD, rxMessage.getText());
+      }
+   }
+
+   private TextMessage receiveMessage() throws Exception {
+      TextMessage rxMessage;
+      try {
+         utx.begin();
+         Session session = connection.createSession(true, Session.AUTO_ACKNOWLEDGE);
+         Destination destination = session.createQueue(TEST_DESTINATION);
+         MessageConsumer consumer = session.createConsumer(destination);
+         rxMessage = (TextMessage) consumer.receive(10L);
+      } finally {
+         utx.commit();
+      }
+      return rxMessage;
    }
 }
