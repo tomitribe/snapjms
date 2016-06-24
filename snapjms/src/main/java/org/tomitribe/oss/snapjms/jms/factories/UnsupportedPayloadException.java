c532632a737f2d99f7da769530ffe1b065526312
Jonathan S. Fisher <jfisher@tomitribe.com>
2016-06-24 11:54:10 -0500

    Add license headers

--- snapjms/src/main/java/org/tomitribe/oss/snapjms/jms/factories/UnsupportedPayloadException.java
+++ snapjms/src/main/java/org/tomitribe/oss/snapjms/jms/factories/UnsupportedPayloadException.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.tomitribe.oss.snapjms.jms.factories;
 
 public class UnsupportedPayloadException extends RuntimeException {
@@ -5,6 +21,7 @@ public class UnsupportedPayloadException extends RuntimeException {
 
    public UnsupportedPayloadException() {
       // TODO update this message as more types are added
-      super("The payload you passed in did not match a pattern to produce a certain message type. It should be a CharSequence (like a String)");
+      super("The payload you passed in did not match a pattern to produce a certain message type."
+            + " It should be a CharSequence (like a String)");
    }
 }
