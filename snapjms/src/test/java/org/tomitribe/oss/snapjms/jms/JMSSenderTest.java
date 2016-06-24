008347268c0bee1054a1d2f90b961ce7c87573b4
Jonathan S. Fisher <jfisher@tomitribe.com>
2016-06-24 11:56:31 -0500

    Add license headers

--- snapjms/src/test/java/org/tomitribe/oss/snapjms/jms/JMSSenderTest.java
+++ snapjms/src/test/java/org/tomitribe/oss/snapjms/jms/JMSSenderTest.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.tomitribe.oss.snapjms.jms;
+ */
+package org.tomitribe.oss.snapjms.jms;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
--- snapjms/src/test/java/org/tomitribe/oss/snapjms/jms/TransactedSessionHolderTest.java
+++ snapjms/src/test/java/org/tomitribe/oss/snapjms/jms/TransactedSessionHolderTest.java
@@ -13,7 +13,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package org.tomitribe.oss.snapjms.jms;
+ */
+package org.tomitribe.oss.snapjms.jms;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.any;
